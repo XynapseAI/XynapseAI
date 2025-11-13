2edfd1271627e705311a443204afa432ba2467c1
XynapseAI <cryptq111222333@gmail.com>
2025-11-13 14:18:41 +0700

    1311 1418

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -315,7 +315,7 @@ function DashboardInner() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [isMounted, setIsMounted] = useState(false);
-  const [activeTab, setActiveTab] = useState('market');
+  const [activeTab, setActiveTab] = useState('profile');
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isAnalyzing, setIsAnalyzing] = useState(false);
   const [providers, setProviders] = useState(null);
